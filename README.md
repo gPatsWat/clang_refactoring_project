633268c8bb0ad8c6a67b226abaee322c80c43785
gspathak <gspathak@uwaterloo.ca>
2022-11-21 14:33:26 -0500

    first commit with working build. integrated googletest

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 /usr/bin/../lib/llvm-10/cmake/
 /usr/bin/../lib/cmake/clang-10/ClangConfig.cmake
 
-cd build; cmake -DCMAKE_INSTALL_PREFIX=../install ..
+cd build
+cmake -DGTEST_ROOT=../googletest ..
+
+References:
+
+Fedor Pikus: C++ Branchless Programming videos
+https://github.com/alpbintug/Branchless-programming/tree/master/basicOperations
---
+++ test/lib/prep_code.h
@@ -0,0 +1,34 @@
+// prep_code.h
+// Feb 06, 2017
+// (c) Copyright 2017 LANSLLC, all rights reserved
+
+#ifndef PREP_CODE_H
+#define PREP_CODE_H
+
+#include "clang/Frontend/ASTUnit.h"
+#include "clang/Tooling/Tooling.h"  //  //buildASTFromCode
+#include "types.h"
+#include "utilities.h"
+
+using ASTUPtr = std::unique_ptr<clang::ASTUnit>;
+
+/**\brief Compile code fragment to AST.
+ * \param code: valid c++ code
+ * \return {unique_ptr<AST>,ASTContext *, TranslationDecl *}
+ *
+ * Will provide -std=c++14 and  Clang include directories as compiler flags.
+ */
+inline auto
+prep_code(corct::str_t_cr code)
+{
+  corct::vec_str args = {"-std=c++14", "-nostdinc++", corct::clang_inc_dir1,
+                         corct::clang_inc_dir2};
+  ASTUPtr ast(clang::tooling::buildASTFromCodeWithArgs(code, args));
+  clang::ASTContext * pctx = &(ast->getASTContext());
+  clang::TranslationUnitDecl * decl = pctx->getTranslationUnitDecl();
+  return std::make_tuple(std::move(ast), pctx, decl);
+}
+
+#endif  // include guard
+
+// End of file
